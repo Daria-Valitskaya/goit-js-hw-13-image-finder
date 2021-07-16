8d414da53ab29b07fa9688869fa9c2e4584e18e9
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-07-16 22:20:40 +0300

    добавлен шаблон карточки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+import { refs } from './js/refs.js';
+import imageCard from './templates/imageCard.hbs';
+// export const refs = {
+//   input: document.querySelector('#search-form'),
+//   searchBtn: document.querySelector('.search-btn'),
+//   gallery: document.querySelector('.gallery'),
+//   loadMore: document.querySelector('[data-action="load-more"]'),
+// };
+
+const renderCard = refs.gallery.insertAdjacentHTML('beforeend', imageCard());
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  input: document.querySelector('#search-form'),
+  searchBtn: document.querySelector('.search-btn'),
+  gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('[data-action="load-more"]'),
+};
